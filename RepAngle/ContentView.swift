65e06cee8e03ce3f670e508af4706cf9804f229c
wilsonckm <wilsonckmchan@gmail.com>
2024-01-25 15:14:38 -0500

    Initial Commit

---
+++ RepAngle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RepAngle
+//
+//  Created by Wilson Chan on 1/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RepAngle/RepAngleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepAngleApp.swift
+//  RepAngle
+//
+//  Created by Wilson Chan on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct RepAngleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

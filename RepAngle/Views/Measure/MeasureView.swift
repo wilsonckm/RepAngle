263c9082621b75e77d3c8323437001c68e4bcd18
wilsonckm <wilsonckmchan@gmail.com>
2024-01-27 15:14:12 -0500

    -In Progress: Refactor to MVVM -Add currentX, currentY, currentZ to allow increased readability and managing values.

--- RepAngle/Views/Measure/MeasureView.swift
+++ RepAngle/Views/Measure/MeasureView.swift
@@ -8,39 +8,43 @@
 import SwiftUI
 
 struct MeasureView: View {
-    @StateObject private var motion = MotionManager()
-    @State private var currentX: Double = 0.0
-    @State private var currentY: Double = 0.0
-    @State private var currentZ: Double = 0.0
-    @State private var initialX: Double = 0.0
-    @State private var initialY: Double = 0.0
-    @State private var initialZ: Double = 0.0
-    @State private var endX: Double = 0.0
-    @State private var endY: Double = 0.0
-    @State private var endZ: Double = 0.0
-    @State private var measurement: Double = 0
+    @StateObject private var viewModel = MeasureViewViewModel()
     
-    @State private var isWithinRange: Bool = false
-    @State private var repCount: Int = 0
-    
-    @State private var currentDate = Date.now
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+//    @State private var currentX: Double = 0.0
+//    @State private var currentY: Double = 0.0
+//    @State private var currentZ: Double = 0.0
+//    @State private var initialX: Double = 0.0
+//    @State private var initialY: Double = 0.0
+//    @State private var initialZ: Double = 0.0
+//    @State private var endX: Double = 0.0
+//    @State private var endY: Double = 0.0
+//    @State private var endZ: Double = 0.0
+//    @State private var measurement: Double = 0
+//    
+//    @State private var isWithinRange: Bool = false
+//    @State private var repCount: Int = 0
+//    
+//    @State private var currentDate = Date.now
+//    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     
     var body: some View {
         
-                if (abs(roundToTenth(value: radiansToDegrees(radians: motion.x)) - endX) <= 6)
-                    && (abs(roundToTenth(value: radiansToDegrees(radians: motion.y)) - endY) <= 6)
-                    && (abs(roundToTenth(value: radiansToDegrees(radians: motion.z)) - endZ) <= 6) {
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(.green)
-                } else {
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(.blue)
-                }
-        Text("\(currentDate)")
-            .onReceive(timer) { input in
-                            currentDate = input
+//        if (abs(roundToTenth(value: radiansToDegrees(radians: motion.x)) - viewModel.endX) <= 6)
+//            && (abs(roundToTenth(value: radiansToDegrees(radians: motion.y)) - viewModel.endY) <= 6)
+//            && (abs(roundToTenth(value: radiansToDegrees(radians: motion.z)) - viewModel.endZ) <= 6) {
+//                    RoundedRectangle(cornerRadius: 25.0)
+//                        .fill(.green)
+//                } else {
+//                    RoundedRectangle(cornerRadius: 25.0)
+//                        .fill(.blue)
+//                }
+        
+        
+        
+        Text("\(viewModel.currentDate)")
+            .onReceive(viewModel.timer) { input in
+                viewModel.currentDate = input
                         }
         
         
@@ -69,9 +73,9 @@ struct MeasureView: View {
                 VStack {
                     Text("Continuous Measurement")
                         .bold()
-                    Text("\(roundToTenth(value:radiansToDegrees(radians:motion.x)), specifier: "%.2f")")
-                    Text("\(roundToTenth(value:radiansToDegrees(radians:motion.y)), specifier: "%.2f")")
-                    Text("\(roundToTenth(value:radiansToDegrees(radians:motion.z)), specifier: "%.2f")")
+                    Text("\(viewModel.currentX, specifier: "%.2f")")
+                    Text("\(viewModel.currentY, specifier: "%.2f")")
+                    Text("\(viewModel.currentX, specifier: "%.2f")")
                 }
                 .padding()
                 
@@ -79,9 +83,9 @@ struct MeasureView: View {
                 VStack {
                     Text("Starting Position")
                         .bold()
-                    Text("\(initialX, specifier: "%.2f")")
-                    Text("\(initialY, specifier: "%.2f")")
-                    Text("\(initialZ, specifier: "%.2f")")
+                    Text("\(viewModel.initialX, specifier: "%.2f")")
+                    Text("\(viewModel.initialY, specifier: "%.2f")")
+                    Text("\(viewModel.initialZ, specifier: "%.2f")")
                 }
                 .padding()
                 
@@ -89,9 +93,9 @@ struct MeasureView: View {
                 VStack {
                     Text("End Position")
                         .bold()
-                    Text("\(endX, specifier: "%.2f")")
-                    Text("\(endY, specifier: "%.2f")")
-                    Text("\(endZ, specifier: "%.2f")")
+                    Text("\(viewModel.endX, specifier: "%.2f")")
+                    Text("\(viewModel.endY, specifier: "%.2f")")
+                    Text("\(viewModel.endZ, specifier: "%.2f")")
                     
                 }
                 .padding()
@@ -99,18 +103,18 @@ struct MeasureView: View {
                 VStack {
                     Text("Measurement")
                         .bold()
-                    Text("\(measurement, specifier: "%.2f")")
+//                    Text("\(viewModel.measurement, specifier: "%.2f")")
                 }
                 
                 VStack {
-                    Text("Rep Count: \(repCount)")
+                    Text("Rep Count: \(viewModel.repCount)")
                 }
                 
                 VStack{
                     Button("Start Position"){
-                        initialX = roundToTenth(value:radiansToDegrees(radians:motion.x))
-                        initialY = roundToTenth(value:radiansToDegrees(radians:motion.y))
-                        initialZ = roundToTenth(value:radiansToDegrees(radians:motion.z))
+//                        viewModel.initialX = roundToTenth(value:radiansToDegrees(radians:motion.x))
+//                        viewModel.initialY = roundToTenth(value:radiansToDegrees(radians:motion.y))
+//                        viewModel.initialZ = roundToTenth(value:radiansToDegrees(radians:motion.z))
                     }
                     .buttonStyle(.borderedProminent)
                     .foregroundStyle(.white)
@@ -118,9 +122,9 @@ struct MeasureView: View {
                     .tint(.green)
                     
                     Button("End Position"){
-                        endX = roundToTenth(value:radiansToDegrees(radians:motion.x))
-                        endY = roundToTenth(value:radiansToDegrees(radians:motion.y))
-                        endZ = roundToTenth(value:radiansToDegrees(radians:motion.z))
+//                        viewModel.endX = roundToTenth(value:radiansToDegrees(radians:motion.x))
+//                        viewModel.endY = roundToTenth(value:radiansToDegrees(radians:motion.y))
+//                        viewModel.endZ = roundToTenth(value:radiansToDegrees(radians:motion.z))
                     }
                     .buttonStyle(.borderedProminent)
                     .foregroundStyle(.white)
@@ -128,7 +132,7 @@ struct MeasureView: View {
                     .tint(.red)
                     
                     Button("Measure"){
-                        measurement = calculateGreatestDifference()
+//                        viewModel.measurement = calculateGreatestDifference()
                     }
                     .buttonStyle(.borderedProminent)
                     .foregroundStyle(.white)
@@ -136,38 +140,6 @@ struct MeasureView: View {
                     .tint(.blue)
                 }
             }
-        func calculateGreatestDifference() -> Double {
-            let differences = [
-                abs(initialX - endX),
-                abs(initialY - endY),
-                abs(initialZ - endZ)
-            ]
-            return differences.max() ?? 0.0
-        }
-        
-        func iterateRep() {
-            repCount += 1
-        }
-        
-        func radiansToDegrees(radians: Double) -> Double {
-            return radians * 180 / .pi
-        }
-        
-        func roundToTenth(value: Double) -> Double {
-            return (value * 10).rounded()/10
-        }
-        
-        func updateRangeStatus() {
-            // Update this to fetch real-time gyroscope data
-            let currentX = roundToTenth(value: radiansToDegrees(radians: motion.x))
-            let currentY = roundToTenth(value: radiansToDegrees(radians: motion.y))
-            let currentZ = roundToTenth(value: radiansToDegrees(radians: motion.z))
-            
-            isWithinRange = (abs(currentX - endX) <= 6)
-            && (abs(currentY - endY) <= 6)
-            && (abs(currentZ - endZ) <= 6)
-        }
-        
     }
     
     //#Preview {
--- RepAngle/Views/Measure/MeasureViewViewModel.swift
+++ RepAngle/Views/Measure/MeasureViewViewModel.swift
@@ -6,3 +6,104 @@
 //
 
 import Foundation
+import CoreMotion
+
+//class MotionManager: ObservableObject {
+//}
+
+class MeasureViewViewModel: ObservableObject {
+    
+    @Published var motionManager = CMMotionManager()
+    
+    //Raw values
+    @Published var x = 0.0
+    @Published var y = 0.0
+    @Published var z = 0.0
+    
+    init() {
+        //Captures data on an interval. Mainly for battery consumption.
+        motionManager.deviceMotionUpdateInterval = 1/15
+        
+        motionManager.startDeviceMotionUpdates(to: .main) { [weak self] data, error in
+            //Data is optional therefore necessary to unwrap
+            guard let motion = data?.attitude else { return }
+            self?.x = motion.roll
+            self?.y = motion.pitch
+            self?.z = motion.yaw
+        }
+    }
+    
+    
+    @Published var initialX: Double = 0.0
+    @Published var initialY: Double = 0.0
+    @Published var initialZ: Double = 0.0
+    @Published var endX: Double = 0.0
+    @Published var endY: Double = 0.0
+    @Published var endZ: Double = 0.0
+//    @Published var measurement: Double = 0
+    @Published var isWithinRange: Bool = false
+    @Published var repCount: Int = 0
+    
+    @Published var currentDate = Date.now
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    
+/*  Formated degrees of continuous measurement.
+    Computed property allows value to recaluclate every time it is accessed
+    Always using current value of self
+ 
+ */
+    
+    var currentX: Double {
+        formatRawValueToDegrees(rawValue: self.x)
+    }
+    var currentY: Double {
+        formatRawValueToDegrees(rawValue: self.y)
+    }
+    
+    var currentZ: Double {
+        formatRawValueToDegrees(rawValue: self.z)
+    }
+    
+    
+    //Formula to format raw value into degrees to tenth place.
+    func formatRawValueToDegrees(rawValue: Double) -> Double {
+        let degrees = radiansToDegrees(radians: rawValue)
+        let rounded = roundToTenth(value: degrees)
+        return rounded
+    }
+    
+    //Raw to degree helper functions
+    func radiansToDegrees(radians: Double) -> Double {
+        return radians * 180 / .pi
+    }
+    
+    
+    func roundToTenth(value: Double) -> Double {
+        return (value * 10).rounded()/10
+    }
+    
+/*  Formula to return the greatest difference between initial and end value.
+ 
+    Use case: To allow user to measure range of motion of a joint
+    Rationale: It is anticipated that the user would move most in a direction they would like to measure
+ */
+    
+    func calculateGreatestDifference() -> Double {
+        let differences = [
+            abs(initialX - endX),
+            abs(initialY - endY),
+            abs(initialZ - endZ)
+        ]
+        return differences.max() ?? 0.0
+    }
+    
+    
+    
+    //To do:
+    func iterateRep() {
+        repCount += 1
+    }
+    
+    
+}
